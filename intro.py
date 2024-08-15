6c88f53c8b4698532b3b04fa57f9e152d2655b03
SrRexxo <133136624+SrRexxo@users.noreply.github.com>
2024-08-15 07:29:44 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -13,7 +13,7 @@ st.write("el texto escribido es", texto)
 
 st.subheader("Ahora usemos 2 columnas")
 
-col1, col2 = st.column(2)
+col1, col2 = st.columns(2)
 
 with col1:
   st.subheader("Esta es la primera columna")
